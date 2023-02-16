6d2a4883faa6d5915f671cda9d6647f71cda5d5c
gshaver <geneshaver@gmail.com>
2023-02-15 18:51:18 -0600

    fixed errors in startup code

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import Home from './screens/home';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font'
+import AppLoading from 'expo-app-loading';
 
 const getFonts = () => Font.loadAsync({
   'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
-  'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+  'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
 });
 
+
 export default function App() {
+
   const [fontsLoaded, setFontsLoaded] = useState(false);
+
 
   if (fontsLoaded) {
     return (
@@ -17,11 +22,23 @@ export default function App() {
     );
   } else {
     return (
-      <AppLoading 
-        startAsync={getFonts} 
-        onFinish={() => setFontsLoaded(true)} 
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={() => console.log('error')}
       />
     )
   }
 
 }
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
